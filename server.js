f9a4e42018105b86a9f8bd58b3ff88891ead9caf
sohailbukhari <sohailbukhari07@gmail.com>
2022-03-26 23:27:08 +0500

    feat: controller

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express');
 
+const WebhookController = require('./src/controller/webhook.controller');
+const { strict } = require('./src/middleware/auth');
+
 const file = require('./package.json');
 const { PORT } = require('./src/config/keys');
-
-const { strict } = require('./src/middleware/auth');
 
 const app = express();
 
 // middlewares
 app.use(express.json());
 
-// routes
-app.get('/', async (req, res) => {
-  res.send({ sucess: true, message: 'PIPELINE ACTIVE', data: { service: file.name, version: file.version, author: file.author, github: file.repository.url } });
+// webhook route
+app.post('/', strict, async (req, res) => {
+  WebhookController.init(req.body);
+  res.send({ success: true, message: 'Build Triggered' });
 });
 
-app.post('/', strict, async (req, res) => {
-  const { headers, body } = req;
-  console.log({ headers, body });
-
-  res.send({ success: true, message: 'Build Triggered' });
+// heartbeat
+app.get('*', async (req, res) => {
+  res.send({ sucess: true, message: 'PIPELINE ACTIVE', data: { service: file.name, version: file.version, author: file.author, github: file.repository.url } });
 });
 
 // server listening
---
+++ src/controller/webhook.controller.js
@@ -0,0 +1,6 @@
+const init = async (payload) => {
+  // todo
+  console.log('BUILD TRIGGERED', { payload });
+};
+
+module.exports = { init };
