1c83d6805992b736ce87264104857e15b9b25c78
sohailbukhari <sohailbukhari07@gmail.com>
2022-03-26 23:44:15 +0500

    feat: exec command

--- src/controller/webhook.controller.js
+++ src/controller/webhook.controller.js
@@ -1,16 +1,21 @@
+const exec = require('child_process').exec;
 const { BRANCH } = require('../config/keys');
 
+const REPO_PATH = '~/source';
+
 const init = async (payload) => {
-  const { ref, pusher, repository } = payload;
-  const { name, html_url } = payload.repository;
+  const { ref, pusher, repository, deleted } = payload;
+  const { name, html_url } = repository;
 
   // trigger against specific branch only
-  if (!ref.includes(BRANCH)) {
+  if (!ref.includes(BRANCH) || deleted) {
     return console.log('BUILD SKIPPED', { BRANCH: ref });
   }
 
-  // todo
-  console.log('BUILD TRIGGERED', { payload });
+  // todo - attach your server repo trigger command here
+  exec(`cd ${REPO_PATH}/${name} && git pull && npm run build && npm run start`);
+
+  console.log('BUILD TRIGGERED', { name, html_url, pusher });
 };
 
 module.exports = { init };
