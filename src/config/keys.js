08c839198a6861e32a7c1e55307f875406455ef4
sohailbukhari <sohailbukhari07@gmail.com>
2022-03-26 23:35:22 +0500

    feat: specific build

--- src/config/keys.js
+++ src/config/keys.js
@@ -5,4 +5,5 @@ dotenv.config();
 module.exports = {
   PORT: process.env.PORT || 3838,
   SECRET: process.env.SECRET || 'admin',
+  BRANCH: 'main',
 };
--- src/controller/webhook.controller.js
+++ src/controller/webhook.controller.js
@@ -1,4 +1,14 @@
+const { BRANCH } = require('../config/keys');
+
 const init = async (payload) => {
+  const { ref, pusher, repository } = payload;
+  const { name, html_url } = payload.repository;
+
+  // trigger against specific branch only
+  if (!ref.includes(BRANCH)) {
+    return console.log('BUILD SKIPPED', { BRANCH: ref });
+  }
+
   // todo
   console.log('BUILD TRIGGERED', { payload });
 };
