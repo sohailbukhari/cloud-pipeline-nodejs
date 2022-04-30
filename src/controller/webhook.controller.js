b0e0593e8b8ee86feb8cf79110e8e67203dd81b4
sohailbukhari <sohailbukhari07@gmail.com>
2022-04-30 15:34:20 +0500

    feat: multi branch deployment

--- src/controller/webhook.controller.js
+++ src/controller/webhook.controller.js
@@ -2,28 +2,39 @@ const exec = require('child_process').exec;
 
 const { BRANCH, REPO_PATH, EXEC_COMMAND } = require('../config/keys');
 
+const BRANCHES = BRANCH.split(',');
+const MULTI_DEPLOYMENT = BRANCHES.length > 1;
+
+console.log({ MULTI_DEPLOYMENT });
+
 const init = async (payload) => {
   const { ref, pusher, repository, deleted } = payload;
   const { name, html_url } = repository;
 
-  // trigger against specific branch only
-  if (!ref || !ref.includes(BRANCH) || deleted) {
-    return console.log('BUILD SKIPPED', { BRANCH: ref });
-  }
-
-  // todo - attach your server repo trigger command here
-  const command = `cd ${REPO_PATH}/${name} && git pull && ${EXEC_COMMAND}`;
-
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`EXEC ERROR: ${error}`);
-      return;
+  BRANCHES.forEach((Branch) => {
+    // trigger against specific branch only
+    if (!ref || !ref.includes(Branch) || deleted) {
+      return console.log('BUILD SKIPPED', { BRANCH: ref });
     }
-    console.log(`STDOUT: ${stdout}`);
-    console.error(`STDERR: ${stderr}`);
-  });
 
-  console.log('BUILD_TRIGGERED: ', { name, html_url, pusher, command });
+    let command = `cd ${REPO_PATH}/${name} && git pull && ${EXEC_COMMAND}`;
+
+    // todo - attach your server repo trigger command here
+    if (MULTI_DEPLOYMENT) {
+      command = `cd ${REPO_PATH}/${name}-${Branch} && git pull && ${EXEC_COMMAND}`;
+    }
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`EXEC ERROR: ${error}`);
+        return;
+      }
+      console.log(`STDOUT: ${stdout}`);
+      console.error(`STDERR: ${stderr}`);
+    });
+
+    console.log('BUILD_TRIGGERED: ', { name, html_url, pusher, command });
+  });
 };
 
 module.exports = { init };
